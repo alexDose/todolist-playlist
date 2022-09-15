57f7d490c8b2432a669a6c3f55c9944e44140165
Alexei <i0ver@mail.ru>
2022-09-15 15:55:25 +0300

    lesson 12 storybook snapshot

--- integration/app.test.js
+++ integration/app.test.js
@@ -8,3 +8,48 @@ describe('addItemForm', () => {
         expect(image).toMatchImageSnapshot();
     });
 });
+
+describe('appWithRedux', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=appwithredux--app-with-redux-example&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
+describe('editableSpan', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=editablespan--editable-span-base-example&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
+describe('taskIsDoneExample', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=task--task-is-done-example&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
+describe('taskIsNotDoneExample', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=task--task-is-not-done-example&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
